d665d5249a96abd3854a5ebe334e562ec18e46a5
Kubinyete <vitorkubinyete@hotmail.com>
2017-05-12 20:51:43 -0300

    Adicionado função para carregar classes, automaticamente importando os arquivos

---
+++ bootstrap/autoload.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Registra a função de auto-carregar classes
+ */
+
+spl_autoload_register(
+	function ($caminhoClasse) {
+		$caminhoClasse = explode('\\', $caminhoClasse);
+
+		// Transforma o nome do primeiro diretório para lowercase
+		// Pois todas as pastas no diretório principal estão em lowercase
+		// App\Database\Class => app\Database\Class
+		if (isset($caminhoClasse[0])) {
+			$caminhoClasse[0] = strtolower($caminhoClasse[0]);
+		}
+
+		$caminhoClasse = implode(DIRECTORY_SEPARATOR, $caminhoClasse);
+
+		require_once dirname(__DIR__).DIRECTORY_SEPARATOR.$caminhoClasse.'.php';
+	}
+);
+
+?>
