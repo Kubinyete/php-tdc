b875d9f7b13359be58937c5d911808bcdcc37d56
Kubinyete <vitorkubinyete@hotmail.com>
2017-05-13 12:07:10 -0300

    Implementando as classes de Grupo & Jogador

---
+++ app/Objetos/Grupo.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+/**
+ * Classe responsável por representar um Grupo de jogadores em uma Aliança
+ */
+
+namespace App\Objetos;
+
+use App\Objetos\Objeto;
+use App\Objetos\Alianca;
+
+class Grupo extends Objeto {
+	// $id
+	// $dataCriacao
+	private $nome;
+	private $alianca;
+
+	public function __construct(int $id, string $dataCriacao, Alianca $alianca, string $nome) {
+		parent::__construct($id, $dataCriacao);
+
+		$this->setAlianca($alianca);
+		$this->setNome($nome);
+	}
+
+	public function getNome() : string { return $this->nome; }
+	public function getAlianca() : Alianca { return $this->alianca; }
+
+	public function setNome(string $valor) { $this->nome = $valor; }
+	public function setAlianca(Alianca $valor) { $this->alianca = $valor; }
+}
+
+?>
---
+++ app/Objetos/Jogador.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+/**
+ * Classe responsável por representar um Jogador em nossa aplicação
+ */
+
+namespace App\Objetos;
+
+use App\Objetos\Objeto;
+use App\Objetos\Alianca;
+
+class Jogador extends Objeto {
+	// $id
+	// $dataCriacao
+	protected $alianca;
+	protected $nome;
+	protected $nickname;
+	protected $nivel;
+	protected $telefone;
+	protected $email;
+	protected $tipo;
+	protected $status;
+	protected $observacoes;
+
+	public function __construct(int $id, string $dataCriacao, Alianca $alianca, string $nome, string $nickname, int $nivel, string $telefone, string $email, int $tipo, bool $status, string $observacoes) {
+		parent::__construct($id, $dataCriacao);
+
+		$this->setAlianca($alianca);
+		$this->setNome($nome);
+		$this->setNickname($nickname);
+		$this->setNivel($nivel);
+		$this->setTelefone($telefone);
+		$this->setEmail($email);
+		$this->setTipo($tipo);
+		$this->setStatus($status);
+		$this->setObservacoes($observacoes);
+	}
+
+	/**
+	 * TODO: Getters & Setters
+	 */
+}
+
+?>
