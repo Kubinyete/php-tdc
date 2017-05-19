ffda003ff0c11f23e213e75a6e27c5ae4e465287
Kubinyete <vitorkubinyete@hotmail.com>
2017-05-19 14:22:56 -0300

    Implementando o objeto PDO em nossa Conexao

--- app/Database/Conexao.php
+++ app/Database/Conexao.php
@@ -6,10 +6,42 @@
 
 namespace App\Database;
 
+use PDO;
+use PDOException;
+use App\Config;
+
 class Conexao {
-	/**
-	 * TODO
-	 */
+	private $conexao;
+
+	public function __construct(?string $stringConexao = null, ?string $usuario, ?string $senha) {
+		try {
+			if ($stringConexao !== null) {
+				$this->conexao = new PDO($stringConexao, $usuario ?? '', $senha ?? '');
+			} else {
+				/**
+				 * Obtenha as carácteristicas padrões de uma conexão definida no arquivo de configurações
+				 */
+				
+				$this->conexao = new PDO(
+					Config::get('database.stringConexao'),
+					Config::get('database.usuario'),
+					Config::get('database.senha')
+				);
+			}
+		} catch (PDOException $e) {
+			exit(
+				'Não foi possível estabelecer uma conexão com o banco de dados.'.
+				'<br>'.
+				'<strong>PDOException:</strong> <em>"'.$e->getMessage().'"</em>.'
+			);
+		}
+	}
+
+	public function query(string $sql) : ?array {
+		$query = $this->conexao->query($sql);
+
+		return (!$query) ? null : $query;
+	}
 }
 
 ?>
