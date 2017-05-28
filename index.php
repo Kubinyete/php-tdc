d51334f231b372f5af18d93148cd021330e78edf
Kubinyete <vitorkubinyete@hotmail.com>
2017-05-28 18:46:02 -0300

    Adicionado o require para rotas.php

--- index.php
+++ index.php
@@ -6,34 +6,41 @@
  */
 
 // Define uma constante global contendo o caminho base da aplicação
-define('APP_BASE', 
-	__DIR__.DIRECTORY_SEPARATOR
-);
+define('APP_BASE', __DIR__.DIRECTORY_SEPARATOR);
 
 // Carregue nosso importador de classes
 require APP_BASE.'bootstrap'.DIRECTORY_SEPARATOR.'autoload.php';
 
 // Carregue nosso arquivo de configurações na memória
-App\Config\AppConfig::carregar(
-	APP_BASE.'bootstrap'.DIRECTORY_SEPARATOR.'config.json'
-);
+App\Config\AppConfig::carregar(APP_BASE.'bootstrap'.DIRECTORY_SEPARATOR.'config.json');
 
 // Define de modo global se estamos com o modo debug ativado
-define('APP_DEBUG', 
-	App\Config\AppConfig::obter('App.ModoDebug') ?? false
-);
+define('APP_DEBUG', App\Config\AppConfig::obter('App.ModoDebug') ?? false);
+
+// Define a base web de requisições
+define('WEB_BASE', App\Config\AppConfig::obter('App.WebBase') ?? '/');
+
+// Registra todas as rotas utilizadas pela aplicação
+require APP_BASE.'bootstrap'.DIRECTORY_SEPARATOR.'rotas.php';
+
+####################################################################################################
 
 // Como o programa vai gerar as datas de criação dos objetos, devemos setar a timezone correta
 // para que a função date() retorne corretamente
-date_default_timezone_set(
-	'America/Sao_Paulo'
-);
+date_default_timezone_set('America/Sao_Paulo');
 
-App\Http\Resposta::header('Content-Type',         'text/html; charset=UTF-8');
-App\Http\Resposta::header('Content-Language',     'pt-BR');
-App\Http\Resposta::header('X-UA-Compatible',      'IE=Edge, chrome=1');
+App\Http\Resposta::header('Content-Type', 'text/html; charset=UTF-8');
+App\Http\Resposta::header('Content-Language', 'pt-BR');
+App\Http\Resposta::header('X-UA-Compatible', 'IE=Edge, chrome=1');
 
 App\Http\Sessao::inicializar();
 App\Http\Sessao::validar();
+
+####################################################################################################
+
+// Sirva nosso pedido HTTP com base na querystring ?r=<pagina>
+// se o valor de 'r' for null, quer dizer que o usuário fez uma simples requisição '/', então assuma
+// que ele queira acessar a página de login
+App\Http\Roteador::servir(App\Http\Pedido::obter('r', App\Http\Pedido::GET) ?? 'login');
 
 ?>
